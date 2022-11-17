dbe551204a5a8683b50447407c290c831f1609bb
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2022-11-17 16:32:23 -0500

    change swagger2 by springfox-boot-starter package

--- src/main/java/com/carlosblinf/todoapp/TodoappApplication.java
+++ src/main/java/com/carlosblinf/todoapp/TodoappApplication.java
@@ -2,9 +2,7 @@ package com.carlosblinf.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @SpringBootApplication
 public class TodoappApplication {
 
